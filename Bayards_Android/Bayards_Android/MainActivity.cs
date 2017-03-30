6d2249f31a97dcd481d82cbdd0e12ca24907745e
CepBuch <cep.buch@gmail.com>
2017-03-30 21:53:18 +0300

    Initial

---
+++ Bayards_Android/Bayards_Android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Bayards_Android
+{
+    [Activity(Label = "Bayards_Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
